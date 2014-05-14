7f6f347a46c3a831b608d41a822ceb9be2f2eeb5
BenchR267 <herzog.benjamin@me.com>
2014-05-14 12:52:54 +0200

    Datenbank auf Server angelegt, in der .sql sind die Befehle, die dafür eingegeben wurden

---
+++ Datenbank/sybase_SE.sql
@@ -0,0 +1,100 @@
+create table Student (
+	sNummer char(6) NOT NULL,
+	Vorname char(15),
+	Nachname char(15),
+	Mail char(50),
+	Rolle char(25),
+	primary key (sNummer)
+)
+
+create table Zuordnung_GruppeStudent (
+	Gruppenkennung char(6) NOT NULL,
+	sNummer char(6) NOT NULL,
+	primary key (Gruppenkennung, sNummer)
+)
+
+create table Gruppe (
+	Gruppenkennung char(6) NOT NULL,
+	Themennummer int NOT NULL,
+	Passwort char(10),
+	primary key(Gruppenkennung)
+)
+
+create table Zuordnung_GruppeBeleg (
+	Gruppenkennung char(6) NOT NULL,
+	Belegkennung char(20) NOT NULL,
+	primary key (Gruppenkennung, Belegkennung)
+)
+
+create table Beleg (
+	Belegkennung char(20) NOT NULL,
+	Semester char(10),
+	StartDatum Date,
+	EndDatum Date,
+	MinAnzMitglieder int,
+	MaxAnzMitglieder int,
+	Passwort char(10),
+	primary key (Belegkennung)
+)
+
+create table Zuordnung_BelegThema (
+	Belegkennung char(20) NOT NULL,
+	Themennummer int NOT NULL,
+	primary key (Belegkennung, Themennummer)
+)
+
+create table Thema (
+	Themennummer int NOT NULL,
+	Aufgabe char(80),
+	primary key (Themennummer)
+)
+
+create table Zuordnung_BelegCase (
+	Belegkennung char(20) NOT NULL,
+	Casekennung char(6) NOT NULL,
+	primary key (Belegkennung, Casekennung)
+)
+
+create table Case (
+	Casekennung char(6) NOT NULL,
+	primary key (Casekennung)
+)
+
+create table Zuordnung_BelegRolle (
+	Belegkennung char(20) NOT NULL,
+	Rolle char(25) NOT NULL,
+	primary key (Belegkennung, Rolle)
+)
+
+create table Rolle (
+	Rolle char(25) NOT NULL,
+	primary key (Rolle)
+)
+
+
+
+
+
+
+
+
+
+
+
+
+ausgeführt:
+
+insert into Student values ("s68311", "Benjamin", "Herzog", "s68311@htw-dresden.de", "Implementation")
+insert into Student values ("s68398", "Markus", "Noack", "s68398@htw-dresden.de", "Datenbankentwurf")
+
+insert into Cases values ("caseXX") // case01-case25
+
+insert into Rolle values("Leitung")
+insert into Rolle values("Entwurf")
+insert into Rolle values("Datenbankentwurf")
+insert into Rolle values("Implementation")
+insert into Rolle values("Analyse")
+insert into Rolle values("Test")
+insert into Rolle values("Dokumentation")
+
+insert into Beleg values ("belegSS14","SS 14","15-05-2014","20-05-2014",6,7,"passwort")
