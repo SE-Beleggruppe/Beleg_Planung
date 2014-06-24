b26ab86af6cb0cf329c65274738e3dd006e30166
Christian Schwarz <schwarz-chr@web.de>
2014-06-24 14:50:44 +0200

    plotting scripts hinzugefügt

---
+++ Testberichte/parse.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+def die(s)
+  puts s
+  exit -1
+end
+
+die "usage: parse.rb <filename>" if not ARGV[0]
+
+ARGV.each do
+|filename|
+  f = File.open filename
+
+  data = /\#\s*([^\n]+)/.match f.readline
+  die "not a test report!" if not data
+#  puts data[1]
+
+  cases = 0
+  fails = -2
+
+  f.each do
+    |s|
+    data = /^\*\s/.match s
+    if data then
+      cases += 1
+      fails += 1 if /\sok\s/.match s
+    end
+  end
+
+  puts cases.to_s + "\t" + fails.to_s + "\t" + (fails * 1000 / cases / 10.0).to_s
+
+  f.close
+end
---
+++ Testberichte/plot.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+d = Dir.open "."
+
+while f = d.read do
+  
+  basename = f.match /(.*)\.txt/i
+  next if not basename
+  basename = basename[1]
+
+  plot = File.open "plot", "w"
+
+  plot.puts "reset"
+  plot.puts "unset xtics"
+
+  lines  = []
+  lw     = []
+  size   = nil
+
+  File.read("settings.conf").scan(/\s*(\S+)\s*{([^}]+)/).each do
+    |group, inner|
+    case group
+    when /^diagramm$/i
+      inner.scan(/(\S*)\s*=\s*([^#]*?)\s*[#\n]/).each do
+        |k,v|
+
+        puts k + " = '" + v + "'"
+        next if /^auto$/i.match v
+
+        case k
+        when /^x$/i
+          plot.puts "set xrange [" + v + "]"
+        when /^xabst$/i
+          plot.puts "set xtics " + v
+        when /^y$/i
+          plot.puts "set yrange [" + v + "]"
+        when /^yabst$/i
+          plot.puts "set ytics " + v
+        when /^größe$/i
+          size = "size " + v
+        when /^breiteMarke$/i
+          lw[0] = v
+        when /^breiteDaten$/i
+          lw[1] = v
+        end
+      end
+    when /^daten$/i
+      inner.scan(/(\S*)\s*=\s*([^#]*?)\s*[#\n]/).each do
+        |k, v|
+        puts "k: " + k + ", v: " + v
+        k = k.match(/^marke$/i) ? nil : ("'" + k + "'")
+        tmp = v.split(":")
+        line = 
+          k.to_s + (k ? "using " + tmp[0] + " with lines": tmp[0]) + 
+          (tmp[1].match(/^auto$/i) ? "" : " lc " + tmp[1].sub("'", "rgb '#")) +
+          " lw " + (k ? lw[1] : lw[0]) +
+          " title '" + tmp[2].to_s + "'"
+        puts "pushing: " + line
+        lines.push line
+      end
+    end
+  end
+
+  plot.puts "set terminal pngcairo enhanced font 'arial' " + size.to_s
+  plot.puts "set output '" + basename + ".png'"
+  plot.print "plot " + (lines * ", ")
+  first = true
+
+  plot.close
+
+  `gnuplot plot`
+
+  File.delete "plot"
+end
+
+d.close
